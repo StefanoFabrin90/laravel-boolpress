6373da247387555096a91abbce7c70facfd4c618
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-31 16:06:44 +0100

    add 3 organizzazione Vue

--- blog/resources/js/front-office.js
+++ blog/resources/js/front-office.js
@@ -1,1 +1,9 @@
-console.log('test');
+//console.log('test');
+
+import Vue from 'vue';
+import App from './views/App';
+
+const root = new Vue({
+    el: '#root',
+    render: h => h(App),
+});
---
+++ blog/resources/js/views/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div class="container text-center mt-5">
+        <h1>Work in Progress</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    component:{}
+}
+</script>
+
+<style lang="scss">
+    .container {
+        h1 {
+            text-transform: uppercase;
+        }
+    }
+</style>
