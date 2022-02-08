9630a060c31dcee7bcf8a815886bccf5d59b0e2a
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-02-08 18:13:24 +0100

    add 2 visualizzazione post

--- blog/resources/js/views/App.vue
+++ blog/resources/js/views/App.vue
@@ -1,15 +1,44 @@
 <template>
   <div>
       <div class="container text-center mt-5">
-        <h1>Work in Progress</h1>
+        <h1 class="mb-5">our Blog</h1>
+        <div v-if="posts">
+            <article v-for="post in posts" :key="`post-${post.id}`">
+                <h2 class="mb-3">{{post.title}}</h2>
+                <p>{{ post.created_at }}</p>
+                <p>{{ post.content }}</p>
+                <hr>
+            </article>
+        </div>
+        <div v-else>
+            <p class="text-center">Loading......</p>
+        </div>
       </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'App',
-    component:{}
+    component:{},
+    data() {
+        return {
+            posts: null,
+        }
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            //console.log('axios');
+            axios.get('http://127.0.0.1:8000/api/posts')
+                .then(response => {
+                    this.posts = response.data
+                });
+        }
+    }
 }
 </script>
 
